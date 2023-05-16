6469e1f8c8151c6534731d80561f4d21cff1196e
KateFront <ekaterina.petrushenko.94@gmail.com>
2023-05-16 15:06:21 +0300

    add property innerHTML

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:31:06 GMT+0300 (Moscow Standard Time)",
         "title": "<b style='font-size: 12px;'>В Сочи суд арестовал блогера</b>, катавшегося примотанным скотчем к машине",
         "description": "Также нарушителю выписаны административные штрафы.",
-        "image": "\"onmouseover=alert('Зло') style='height: 50px'\"",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 5
     }, {
@@ -25,7 +25,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:30:00 GMT+0300 (Moscow Standard Time)",
         "title": "В Киеве рассказали об опасности переименования Украины",
         "description": "Как считает экс-депутат, «необходимость борьбы за слово “Русь” была в 19 веке».",
-        "image": "https://rusvesna.su/sites/default/files/trezub_ukraina.jpg",
+        "image": "./images/image.jpg",
         "source_id": 11,
         "category_id": 5
     }, {
@@ -34,7 +34,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:28:00 GMT+0300 (Moscow Standard Time)",
         "title": "Motorola представила смартфоны Edge 20 Business Edition и Edge 20 Lite Business Edition с повышенной безопасностью",
         "description": "Motorola представила смартфоны Edge 20 Business Edition и Edge 20 Lite Business Edition. Новые версии имеют такое же оснащение, что и обычные версии, однако имеют лучшие возможности для обеспечения безопасности пользовательских данных. Стоит отметить, что Motorola Edge 20 Pro Business Edition вышел некоторое время назад.",
-        "image": "https://3dnews.ru/assets/external/illustrations/2021/09/02/1048219/3289328.jpg",
+        "image": "./images/image.jpg",
         "source_id": 6,
         "category_id": 1
     }, {
@@ -43,7 +43,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:23:31 GMT+0300 (Moscow Standard Time)",
         "title": "Россия не продлит мандат ОБСЕ на пунктах пропуска на границе с Украиной",
         "description": "Россия не видит оснований для того, чтобы продлить мандат группы наблюдателей ОБСЕ на пунктах пропуска \"Гуково\" и \"Донецк\" на границе с Украиной после 30 сентября. Об этом заявил постпред РФ при организации Александр ...",
-        "image": "https://img.gazeta.ru/files3/870/12803870/upload-RIAN_6068011.HR-pic905-895x505-39.jpg",
+        "image": "./images/image.jpg",
         "source_id": 15,
         "category_id": 4
     }, {
@@ -52,7 +52,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:20:19 GMT+0300 (Moscow Standard Time)",
         "title": "Зеленский назвал Украину мировым \"ИТ-хабом\"",
         "description": "Президент Украины Владимир Зеленский сообщил после встречи с гендиректором Apple Тимом Куком, что Киев заинтересован в новых совместных проектах. Об этом заявляется в Twiiter-профиле украинского главы.\n\n\"Рад встретиться с Тимом Куком, ...",
-        "image": "https://img.gazeta.ru/files3/748/13945748/E-TR-e1WQA4Qz7h-pic905-895x505-94848.jpg",
+        "image": "./images/image.jpg",
         "source_id": 15,
         "category_id": 4
     }, {
@@ -61,7 +61,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:15:41 GMT+0300 (Moscow Standard Time)",
         "title": "Адвокат Костюшкина раскрыл доказательства плагиата в песне Монеточки",
         "description": "Певица уже дважды пропустила суд по делу о нарушении авторских прав экс-участника группы «Чай вдвоем».",
-        "image": "https://aif-s3.aif.ru/images/024/948/bdd4ec42ce165cc3b9797e80b0b85b36.jpg",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 3
     }, {
@@ -70,7 +70,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:05:01 GMT+0300 (Moscow Standard Time)",
         "title": "Зеленский пообещал американцам, что Украина станет «лидером Европы»",
         "description": "В рамках рабочей поездки в США украинский президент 2 сентября выступил в Стэндфордском университете.",
-        "image": "https://aif-s3.aif.ru/images/024/948/0a7b249b2cecc221c01e6c0538de3fdd.jpeg",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 4
     }, {
@@ -79,7 +79,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:02:34 GMT+0300 (Moscow Standard Time)",
         "title": "Глава МИД Австрии назвал условие для признания \"Талибана\"",
         "description": "Министр иностранных дел Австрии Александер Шалленберг по прибытии на неформальную встречу главы МИД стран Евросоюза в Словении заявил о том, возможное признание радикального движения \"Талибан\" со стороны международного сообщества ...",
-        "image": "https://img.gazeta.ru/files3/800/13938800/AP21243461700365-pic905-895x505-5182.jpg",
+        "image": "./images/image.jpg",
         "source_id": 15,
         "category_id": 4
     }, {
@@ -88,7 +88,7 @@ const data = {
         "date": "Thu Sep 02 2021 23:00:17 GMT+0300 (Moscow Standard Time)",
         "title": "Попова рассказала о ситуации с новыми штаммами коронавируса в РФ",
         "description": "Глава Роспотребнадзора не исключила, что осенью в стране может начаться подъём заболеваемости.",
-        "image": "https://aif-s3.aif.ru/images/024/948/e6bcdb89be63b2b45c0cf7627a5e343c.jpg",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 5
     }, {
@@ -97,7 +97,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:59:47 GMT+0300 (Moscow Standard Time)",
         "title": "Гай Германика прокомментировала задержание мужа в столичном отеле",
         "description": "Режиссер Валерия Гай Германика отреагировала на задержание своего мужа, бизнесмена Дениса Молчанова. Об этом сообщает издание Starhit.\n\n\"У богатых свои причуды, но наркотиков точно нет\", - объяснила российский режиссер.\n\nОна ...",
-        "image": "https://img.gazeta.ru/files3/907/12216907/upload-RIAN_2453813.HR-pic905-895x505-67565.jpg",
+        "image": "./images/image.jpg",
         "source_id": 15,
         "category_id": 3
     }, {
@@ -106,7 +106,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:55:33 GMT+0300 (Moscow Standard Time)",
         "title": "Попова рассказала, сколько надо ждать между прививками от гриппа и COVID",
         "description": "По словам главы Роспотребнадзора, выждать надо не менее четырех недель.",
-        "image": "https://aif-s3.aif.ru/images/024/948/82ae8d058238386c241baa51723df142.jpg",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 5
     }, {
@@ -115,7 +115,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:55:11 GMT+0300 (Moscow Standard Time)",
         "title": "Украина станет лидером Европы, — Зеленский",
         "description": "Президент Украины сделал очередное абсурдное заявление.",
-        "image": "https://rusvesna.su/sites/default/files/zelenskiy_vladimir_0.jpg",
+        "image": "./images/image.jpg",
         "source_id": 11,
         "category_id": 5
     }, {
@@ -124,7 +124,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:55:00 GMT+0300 (Moscow Standard Time)",
         "title": "Предстоящая VR/AR-гарнитура Apple потребует подключения к iPhone",
         "description": "Первую VR/AR-гарнитуру Apple, которая сейчас находится в разработке, необходимо будет подключать по беспроводной сети к iPhone или другому устройству Apple, чтобы получить полную функциональность. Об этом сообщило издание The Information. Гарнитура будет полагаться на вычислительные мощности подключённого устройства Apple.",
-        "image": "https://3dnews.ru/assets/external/illustrations/2021/09/02/1048218/89489234.jpg",
+        "image": "./images/image.jpg",
         "source_id": 6,
         "category_id": 1
     }, {
@@ -133,7 +133,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:47:41 GMT+0300 (Moscow Standard Time)",
         "title": "Центробанк описал сценарий глобального финансового кризиса в 2023 году",
         "description": "Базовый прогноз и три альтернативных сценария от Центробанка.",
-        "image": "https://rusvesna.su/sites/default/files/kopilka_dollar_dengi_finansy.jpg",
+        "image": "./images/image.jpg",
         "source_id": 11,
         "category_id": 5
     }, {
@@ -142,7 +142,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:45:09 GMT+0300 (Moscow Standard Time)",
         "title": "Минпросвещения запустило конкурс «Директор года России»",
         "description": "Подать заявку на участие в конкурсе может любой директор школы со стажем работы свыше трех лет.",
-        "image": "https://aif-s3.aif.ru/images/024/948/fcd96fd75c4008e037c6ec8aaaf797b8.jpg",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 4
     }, {
@@ -151,7 +151,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:41:08 GMT+0300 (Moscow Standard Time)",
         "title": "Сезон Tempest в For Honor стартует&nbsp;9 сентября",
         "description": "Глава Mirage будет закрыта.",
-        "image": "https://cdn.igromania.ru/mnt/news/e/d/9/5/6/b/108303/b39e8cacacdc15b6_848x477.jpg",
+        "image": "./images/image.jpg",
         "source_id": 9,
         "category_id": 1
     }, {
@@ -160,7 +160,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:40:41 GMT+0300 (Moscow Standard Time)",
         "title": "В офисе Зеленского заявили о готовности США \"помочь\" завершить конфликт на востоке Украины",
         "description": "Соединенные Штаты в ходе визита Владимира Зеленского в Вашингтон подтвердили готовность помочь Украине завершить \"войну\" и обеспечить систему безопасности страны. Об этом в эфире телеканала \"1+1\" заявил глава офиса ...",
-        "image": "https://img.gazeta.ru/files3/322/13933322/2in1-2021new-pic905-895x505-95738.jpg",
+        "image": "./images/image.jpg",
         "source_id": 15,
         "category_id": 4
     }, {
@@ -169,7 +169,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:39:06 GMT+0300 (Moscow Standard Time)",
         "title": "Virgin Galactic Брэнсона запретили устраивать полеты в космос",
         "description": "Компании Virgin Galactic британского предпринимателя Ричарда Брэнсона временно запретили организовывать полеты в космос. Об этом сообщает FOX Business.\n\nПо информации Федерального управления гражданской авиации США, у Virgin Galactic не ...",
-        "image": "https://img.gazeta.ru/files3/900/13721900/upload-2021-07-11T161320Z_701195493_HP1EH7B18UQ0N_RTRMADP_3_SPACE-EXPLORATION-VIRGINGALACTIC-pic905-895x505-70481.jpg",
+        "image": "./images/image.jpg",
         "source_id": 15,
         "category_id": 4
     }, {
@@ -178,7 +178,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:35:58 GMT+0300 (Moscow Standard Time)",
         "title": "Полузащитник ЦСКА Марадишвили стал игроком «Локомотива»",
         "description": "Футболист сыграл за ЦСКА 49 матчей, забив два мяча.",
-        "image": "https://aif-s3.aif.ru/images/024/948/8a6d36735e7ee1c06dabeceaa24c37e6.jpg",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 2
     }, {
@@ -187,7 +187,7 @@ const data = {
         "date": "Thu Sep 02 2021 22:23:28 GMT+0300 (Moscow Standard Time)",
         "title": "В Российском движении школьников сочли оговорку Путина неслучайной",
         "description": "Ранее ученик из Воркуты поправил президента во время открытого урока истории во Владивостоке.",
-        "image": "https://aif-s3.aif.ru/images/024/948/b3431532fdc9c910a45338dd31c85726.jpg",
+        "image": "./images/image.jpg",
         "source_id": 14,
         "category_id": 4
     }]
@@ -199,78 +199,50 @@ const smallNews = data.items.slice(3, 12);
 const mainNewsContainer = document.querySelector('.articles__big-column');
 const smallNewsContainer = document.querySelector('.articles__small-column');
 
-
-const createMainNewsItem = (item) => {
-    const categoryData = data.categories.find((categoryItem) => categoryItem.id === item.category_id);
-    const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
-
-    const article = document.createElement('article');
-    const imageContainer = document.createElement('div');
-    const image = document.createElement('img');
-    const content = document.createElement('div');
-    const category = document.createElement('span');
-    const title = document.createElement('h2');
-    const text = document.createElement('p');
-    const source = document.createElement('span');
-
-    article.classList.add('main-article');
-    imageContainer.classList.add('main-article__image-container');
-    image.classList.add('main-article__image');
-    content.classList.add('main-article__content');
-    category.classList.add('article-category', 'main-article__category');
-    title.classList.add('main-article__title');
-    text.classList.add('main-article__text');
-    source.classList.add('article-source', 'main-article__source');
-
-    title.textContent = item.title;
-    image.src = item.image;
-    category.textContent = categoryData.name;
-    text.textContent = item.description;
-    source.textContent = sourceData.name;
-
-    imageContainer.appendChild(image);
-    article.appendChild(content);
-    content.appendChild(category);
-    content.appendChild(title);
-    content.appendChild(text);
-    content.appendChild(source);
-    article.appendChild(content);
-
-    return article;
-}
-
-const createSmallNewsItem = (item) => {
-    const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
-    const dateData = new Date(item.date).toLocaleDateString('ru-Ru', {month: 'long', day: 'numeric'});
-
-    const article = document.createElement('article');
-    const title = document.createElement('h2');
-    const textContainer = document.createElement('p');
-    const source = document.createElement('span');
-    const date = document.createElement('span');
-
-    article.classList.add('small-article');
-    title.classList.add('small-article__title');
-    textContainer.classList.add('small-article__text');
-    source.classList.add('article-source', 'small-article__source');
-    date.classList.add('article-date', 'small-article__date');
-
-    title.textContent = item.title;
-    source.textContent = sourceData.name;
-    date.textContent = dateData.name;
-
-    article.appendChild(title);
-    article.appendChild(textContainer);
-    textContainer.appendChild(date);
-    textContainer.appendChild(source);
-
-    return article;
+const escapeString = (string) => {
+    const symbols = {
+        '&': '&amp;',
+        '<': '&lt:',
+        '>': '&gt:'
+    }
+    return string.replace(/[&<>]/g, (tag) => {
+        return symbols[tag] || tag;
+    })
 }
 
 mainNews.forEach((item) => {
-    mainNewsContainer.appendChild(createMainNewsItem(item));
+    const template = document.createElement('template');
+    const categoryData = data.categories.find((categoryItem) => categoryItem.id === item.category_id);
+    const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
+
+    template.innerHTML = `<article class="main-article">
+    <div class="main-article__image-container">
+        <img class="main-article__image" src="${encodeURI(item.image)}" alt="Фото новости">
+    </div>
+    <div class="main-article__content">
+        <span class="article-category main-article__category">${escapeString(categoryData.name)}</span>
+        <h2 class="main-article__title">${escapeString(item.title)}</h2>
+        <p class="main-article__text">${escapeString(item.description)}</p>
+        <span class="article-source main-article__source">${escapeString(sourceData.name)}</span>
+    </div>
+</article>`;
+
+    mainNewsContainer.appendChild(template.content);
 });
 
 smallNews.forEach((item) => {
-    smallNewsContainer.appendChild(createSmallNewsItem(item));
+    const template = document.createElement('template');
+    const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
+    const dateData = new Date(item.date).toLocaleDateString('ru-Ru', {month: 'long', day: 'numeric'});
+
+    template.innerHTML = `<article class="small-article">
+    <h2 class="small-article__title">${escapeString(item.title)}</h2>
+    <p class="small-article__caption">
+        <span class="article-date small-article__date">${dateData}</span>
+        <span class="article-source small-article__source">${escapeString(sourceData.name)}</span>
+    </p>
+</article>`;
+
+
+    smallNewsContainer.appendChild(template.content);
 })
